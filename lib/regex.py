650ccd5350bf83f3d1e231371c6758faa484f9ff
ErickMwangi <rickthetri20@gmail.com>
2023-05-26 00:52:28 +0300

    completed assignment

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,22 @@
 import re
 
-my_pattern = r""
+my_pattern = r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)"
 my_regex = re.compile(my_pattern)
 
+# Test strings
+strings = [
+    "It's such a lovely day today.",
+    "Some weather we're having today, huh?",
+    "Maybe today's just not my day.",
+    "This is a different string."
+]
+
+# Find matches
+matches = [string for string in strings if my_regex.search(string)]
+
+# Assert matches
+assert matches == [
+    "It's such a lovely day today.",
+    "Some weather we're having today, huh?",
+    "Maybe today's just not my day."
+]
